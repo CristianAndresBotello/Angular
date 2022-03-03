6ca8ee7e43175e130f923ffab41ed2cdba73c78e
unknown <desarrollo01@inversionesarar.com>
2022-03-02 22:37:05 -0500

    Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { EjemplosPipesComponent } from './ejemplos-pipes/ejemplos-pipes.component';
+import { TextoComponent } from './texto/texto.component';
+import { MayusculasPipe } from './pipes/mayusculas.pipe';
 
 @NgModule({
+  // Componentes, Pipes
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    EjemplosPipesComponent,
+    TextoComponent,
+    MayusculasPipe
   ],
+  // (LO QUE NECESITE ANGULAS - LIBRERIAS)
   imports: [
     BrowserModule,
     AppRoutingModule
